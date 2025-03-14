f5a8bddc9a49b8f561e114f5b8d09a841749e474
Guilherme Costa <guiccosta04@gmail.com>
2025-03-14 15:50:09 +0000

    added new words and some comments. renamed some files. end of project maintenance (hopefully)

--- cron.js
+++ cron.js
@@ -1,23 +1,24 @@
 const { CronJob } = require("cron");
 const https = require("https");
 
+// Define qual é o backend em questão
 const backendUrl = "https://diciomeunario-api.onrender.com/data";
 
 const job = new CronJob("*/14 * * * *", function () {
-  console.log("Attempting cron job");
+  console.log("A tentar cron job");
 
   https
     .get(backendUrl, (res) => {
       if (res.statusCode === 200) {
-        console.log("Cron job worked");
+        console.log("O cron job funcionou!");
       } else {
         console.error(
-          `Cron job couldn't access backend. Status code: ${res.statusCode}`
+          `O cron job não conseguiu aceder ao backend. Código de erro ${res.statusCode}`
         );
       }
     })
     .on("error", (err) => {
-      console.error("Cron job didn't work", err.message);
+      console.error("O cron job deu erro", err.message);
     });
 });
 
